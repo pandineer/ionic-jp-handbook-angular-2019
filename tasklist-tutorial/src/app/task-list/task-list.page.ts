d1f0bd4df79ad699242bb59e22a0168cb27ca200
pandineer <pandineer@gmail.com>
2021-06-15 17:15:32 +0900

    load tasks from localStorage on task-list

--- tasklist-tutorial/src/app/task-list/task-list.page.ts
+++ tasklist-tutorial/src/app/task-list/task-list.page.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./task-list.page.scss'],
 })
 export class TaskListPage implements OnInit {
-
+  tasks: { name: string }[] = [
+    { name: 'タスク１' },
+    { name: 'タスク２' },
+  ];
   constructor() { }
 
   ngOnInit() {
   }
 
+  ionViewWillEnter() {
+    if ('tasks' in localStorage) {
+      this.tasks = JSON.parse(localStorage.tasks);
+    }
+  }
 }
