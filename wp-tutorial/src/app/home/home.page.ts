8c5f943649570eab67be2af19ea11d1e7b933ad2
pandineer <pandineer@gmail.com>
2021-06-15 18:51:03 +0900

    add loading indicator

--- wp-tutorial/src/app/home/home.page.ts
+++ wp-tutorial/src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -16,12 +17,18 @@ export class HomePage {
 
   constructor(
       public http: HttpClient,
+      public loadingController: LoadingController,
   ) {}
 
-  ionViewDidEnter() {
+  async ionViewDidEnter() {
+    const loading = await this.loadingController.create({
+      message: 'Loading...',
+    });
+    await loading.present();
     this.http.get('https://public-api.wordpress.com/rest/v1.1/sites/ionicjp.wordpress.com/posts/')
         .subscribe(data => {
           this.posts = data['posts'];
+          loading.dismiss();
         });
   }
 }
