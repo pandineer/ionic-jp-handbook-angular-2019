b9667732b1e4d3820193bf9c89aa415e8a9051da
pandineer <pandineer@gmail.com>
2021-06-15 17:32:30 +0900

    add actionsheet into tasks on task list

--- tasklist-tutorial/src/app/task-list/task-list.page.ts
+++ tasklist-tutorial/src/app/task-list/task-list.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-task-list',
@@ -10,7 +11,9 @@ export class TaskListPage implements OnInit {
     { name: 'タスク１' },
     { name: 'タスク２' },
   ];
-  constructor() { }
+  constructor(
+      public actionSheetController: ActionSheetController,
+  ) { }
 
   ngOnInit() {
   }
@@ -19,5 +22,35 @@ export class TaskListPage implements OnInit {
     if ('tasks' in localStorage) {
       this.tasks = JSON.parse(localStorage.tasks);
     }
+  }
+
+  async changeTask() {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'タスクの変更',
+      buttons: [
+        {
+          text: '削除',
+          role: 'destructive',
+          icon: 'trash',
+          handler: () => {
+            console.log('Destructive clicked')
+          }
+        }, {
+          text :'変更',
+          icon: 'create',
+          handler: () => {
+            console.log('Archive clicked');
+          }
+        }, {
+          text: '閉じる',
+          icon: 'close',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        },
+      ]
+    });
+    actionSheet.present();
   }
 }
